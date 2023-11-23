b54d7c73fbcd827d8537efc6463b0fd573fb8a43
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-11-23 17:04:07 -0500

    Update code_2.ps1

--- 2015/day1/code_2.ps1
+++ 2015/day1/code_2.ps1
@@ -2,6 +2,7 @@ $input = (Get-Content input.txt).toCharArray()
 $floor = 0
 $pos = 1
 
+# I couldn't make this work using a normal for loop for some reason. Powershell's gimmicky syntax eludes my comprehension.
 foreach ($paren in $input)
 {
     if ($paren -eq "(")
